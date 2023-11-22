1a33a09e4df9f3a228eb4f52ef6b16b7d3e3eee7
Papa Matar Dieng <papamatar.dieng@atlanticdigitalsolutions-sn.com>
2023-11-22 11:40:56 +0000

    SERVER JS AJOUT

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/gestion-empl'));
+app.use(express.static('./dist/gestion-empl-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/'}),
+    res.sendFile('index.html', {root: 'dist/gestion-empl-front'}),
 );
 
 // Start the app by listening on the default Heroku port
